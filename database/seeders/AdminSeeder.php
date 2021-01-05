380f293a48afe3e38e75c232da25ae6a0a4d39c5
Martin-Software-Engineer <71181345+Martin-Software-Engineer@users.noreply.github.com>
2021-01-05 15:50:15 +0800

    Admin Seeder
    admin seeder for all 3 admins

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // DB::table('users')->insert([
+        //     'name' => 'Rey Martin',
+        //     'email' => 'remorrey24@gmail.com',
+        //     'password' => Hash::make('password'),
+        // ]);
+        $AdminRey = User::create([
+            'name' => 'Rey',
+            'email' =>'remorrey24@gmail.com',
+            'password' => Hash::make('12341234'),   
+        ]);
+        
+        $AdminNicole = User::create([
+            'name' => 'Nicole',
+            'email' =>'n@gmail.com',
+            'password' => Hash::make('12341234'),   
+        ]);
+
+        $AdminPat = User::create([
+            'name' => 'Pat',
+            'email' =>'p@gmail.com',
+            'password' => Hash::make('12341234'),   
+        ]);
+        
+        // $roles = Role::where('id',[1])->get();
+        $AdminRey->roles()->attach(1);
+        $AdminNicole->roles()->attach(1);
+        $AdminPat->roles()->attach(1);
+
+        // $AdminRey->attach($roles->random(1)->pluck('id'));
+        // $AdminNicole->attach($roles->random(1)->pluck('id'));
+        // $AdminPat->attach($roles->random(1)->pluck('id'));
+
+        // $roles = Role::where('id',[1])->get();
+
+        // User::whereIn('email',['r@gmail.com','n@gmail.com','p@gmail.com'])->each( function ($user) use ($roles){
+        //     $user->roles()->attach($roles->random(1)->pluck('id'));
+        // });
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UserSeeder::class);
         $this->call(RoleSeeder::class);
         $this->call(RoleUserSeeder::class);
+        $this->call(AdminSeeder::class);
     }
 }
