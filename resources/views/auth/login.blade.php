bea3ecd1688c3f1c0912610e9cffa1fdb0ff3f5d
pmsgacusan <pmsgacusan@gmail.com>
2021-01-21 16:59:22 +0800

    Update login.blade.php error

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,7 +15,8 @@
 
                 <h3 class="mt-2 pd-2">Login</h3>
                 <form method="POST" action="{{ route('login') }}">
-                    @csrf   
+
+                    <!-- @csrf
                     <div class="form-group">
                         <label for="email">Email address</label>
                         <input name="email" type="email" class="form-control @error('email') is-invalid @enderror" id="email" aria-describedby="email" placeholder="Enter Email" value="{{ old('email') }}">
@@ -28,7 +29,8 @@
                         <input name="password" type="password" class="form-control @error('password') is-invalid @enderror" id="password" placeholder="Password">
                         @error('password')
                             <span class="invalid-feedback" role="alert">{{ $message }}</span>
-                        @enderror
+                        @enderror -->
+
                     </div>
                     <button type="submit" class="btn btn-primary mt-4">Login</button>
                 </form>
