64d06a95ca03707fbabbdb81ca4de601808fdcac
Martin-Software-Engineer <71181345+Martin-Software-Engineer@users.noreply.github.com>
2021-05-26 22:31:05 +0800

    update campaigns, services, campaign_view

--- resources/views/jobseeker/contents/service-orders.blade.php
+++ resources/views/jobseeker/contents/service-orders.blade.php
@@ -31,7 +31,7 @@
                     <tr>
                         <th></th>
                         <th>Order ID</th>
-                        <th>Title/Title</th>
+                        <th>Title</th>
                         <th>Date of Service</th>
                         <th>Category</th>
                         <th>Price</th>
--- resources/views/landing/partials/footer.blade.php
+++ resources/views/landing/partials/footer.blade.php
@@ -1,5 +1,41 @@
 <!-- footer section end -->
       <!-- copyright section start -->
+      <div class="section_5_footernav layout_padding_footernav">
+         <div class="container">
+            <div class="row">
+               <div class="col-sm-3 d-flex justify-content-start align-items-start">
+                  <div class="about_img"><img src="{{asset('app-assets/images/additional_pictures/navbar_logo.png')}}"></div>
+               </div>
+               <div class="col-sm-3 d-flex justify-content-start align-items-start ">
+                  <ul>
+                     <li><h4 class="footernav_heading_text">CAMPAIGNS</h4></li>
+                     <li>Education</li>
+                     <li>Medical and Health</li>
+                     <li>Animals</li>
+                     <li>Non-Profit and Charity</li>
+                     <li>Memorial and Funeral</li>
+                     <li>Emergencies</li>
+                  </ul>
+               </div>
+               <div class="col-sm-3 d-flex justify-content-start align-items-start ">
+               <ul>
+                     <li><h4 class="footernav_heading_text">SERVICES</h4></li>
+                     <li>Education</li>
+                     <li>Personal & Home Care</li>
+                     <li>Storekeeper</li>
+                     <li>Food</li>
+                  </ul>
+               </div>
+               <div class="col-sm-3 d-flex justify-content-start align-items-start ">
+               <ul>
+                     <li><h4 class="footernav_heading_text">How it works ?</h4></li>
+                     <li>About Us</li>
+                     <li>What is crowdfunding ?</li>
+                  </ul>
+               </div>
+            </div>
+         </div>
+      </div>
       <div class="copyright_section">
         <div class="container">
            <p class="copyright_text">2020 All Rights Reserved. Design by <a href="https://html.design">Free html  Templates</a></p>
