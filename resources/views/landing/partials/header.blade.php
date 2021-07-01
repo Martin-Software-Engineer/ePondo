a49908bd4f046ac03b727e7a3d6cd9c19f177cc0
mndsleynes <74725573+mndsleynes@users.noreply.github.com>
2021-07-02 00:28:07 +0800

    backer edit

--- resources/views/landing/partials/header.blade.php
+++ resources/views/landing/partials/header.blade.php
@@ -31,12 +31,14 @@
                         <a class="nav-link" href="{{route('admin.index')}}">My Account</a>
                      @endif
                      @if(auth()->user()->hasAnyRole('JobSeeker'))
-                        <!-- <a class="account_btn" href="{{route('jobseeker.index')}}">My Account</a> -->
-                        <a class="nav-link" href="{{route('jobseeker.index')}}">My Account</a>
+           <!-- <a class="account_btn" href="{{route('jobseeker.index')}}">My Account</a> -->
+                     <p>Hello,<a style="display:inline-block" class="nav-link" href="{{route('jobseeker.index')}}">
+                     {{auth()->user()->username}}</a></p>
                      @endif
                      @if(auth()->user()->hasAnyRole('Backer'))
                         <!-- <a class="account_btn" href="{{route('backer.index')}}">My Account</a> -->
-                        <a class="nav-link" href="{{route('backer.index')}}">My Account</a>
+                        <p>Hello,<a style="display:inline-block" class="nav-link" href="{{route('backer.index')}}">
+                     {{auth()->user()->username}}</a></p>
                      @endif
                   @endauth
                   
