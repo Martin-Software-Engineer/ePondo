525aa70e3c3a005fc339d8501e11c6955661d86c
Martin-Software-Engineer <71181345+Martin-Software-Engineer@users.noreply.github.com>
2021-10-17 21:42:07 +0800

    Updates
    1. Added Campaing Status
    2. Social Media links into footer
    3. Invoice edit frontend
    4. Orders-View edit frontend
    5. chats, change epondo logo on tab
    6.  Edit instructions on Campaigns, services, claim earnings, claim funds,
    7. how it works edit frontend
    8. banner edit frontend
    9. landing campaigns and services, added sorry no results found, for search

--- resources/views/landing/contents/campaign_view.blade.php
+++ resources/views/landing/contents/campaign_view.blade.php
@@ -185,11 +185,13 @@
         <div class="row">
             <div class="col-sm-12">
                 <h1 class="campaign_title">{{$campaign->title}}</h1>
-                <span class="campaign_category">
+                <h1>
+                    <span class="s_hlocation"> Category: </span> 
                     @foreach($campaign->categories as $category)
-                        {{$category->name}} @if(!$loop->last)/@endif
+                        <span class="badge badge-info" style="background-color:#120a78;font-size:14px;">{{$category->name}}</span> @if(!$loop->last)@endif
                     @endforeach
-                </span>
+                </h1>
+                    
                 <div class="row">
                     <div class="col-sm-8">
                         <div class="progress-wrapper campaign_progress_bar">
@@ -212,7 +214,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-sm-4 campaign_donate_btn">
+                    <div class="col-sm-4 campaign_donate_btn mb-2">
                         <div class="donate_btn btn btn-block " data-campaign-id="{{$campaign->id}}">
                             <h2 class="donate_now"><img src="{{asset('app-assets/images/additional_pictures/icon-4.png')}}" class="donate_now_img">Donate</h2>
                         </div>
