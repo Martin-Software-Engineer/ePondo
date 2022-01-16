d719362ab40843490bea9a4f7b0d14300a0533b0
Martin-Software-Engineer <71181345+Martin-Software-Engineer@users.noreply.github.com>
2022-01-16 17:40:52 +0800

    ePondo Taskade 10.0 (12)
    Add link to jpeg image

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -5,7 +5,9 @@
 <div class="misc-wrapper">
     <div class="misc-inner p-2 p-sm-3">
         <div class="w-100 text-center">
-            <img style="" class="img_header_email"src="{{asset('app-assets/images/additional_pictures/navbar_logo.png')}}">
+            <a href="/">
+                <img style="" class="img_header_email"src="{{asset('app-assets/images/additional_pictures/navbar_logo.png')}}">
+            </a>
             <h2 class="mb-1">Please verify email address</h2>
             <p class="mb-3">We have sent you a verification link, please check your email inbox.<br>
                 If you have not received any email from us. Please click the button below.</p>
